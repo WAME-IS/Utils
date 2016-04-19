6a4c5215fc7238dc26aefee119bc1ba7f70e8242
shadd0w <rene.gmitter@wame.sk>
2016-04-19 17:37:36 +0200

    Update CategoryModule

---
+++ README.md
@@ -0,0 +1,17 @@
+# Utils
+
+
+## Slug
+
+
+### Use
+
+    Slug::generate(Slug::TITLE, [$name]);
+
+    Slug::generate('[id]-[title]', $id, $title);
+
+
+### Methods
+- generate()
+- generateByTitle()
+- generateByPattern()
---
+++ Slug.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Wame\Utils;
+
+use Nette\Utils\Strings;
+
+class Slug
+{
+	const TITLE = 0;
+	
+	/**
+	 * Generate
+	 * 
+	 * @param integer $type		type
+	 * @param variadic $values	values
+	 */
+	public static function generate($type, ...$values)
+	{
+		switch($type) {
+			case self::TITLE:
+				return createTitle($values[0]);
+			default:
+				return createRegex($type, $values);
+		}
+	}
+	
+	/**
+	 * Generate by title
+	 * 
+	 * @param string $title		title
+	 */
+	public static function generateByTitle($title)
+	{
+		return Strings::webalize($title);
+	}
+	
+	/**
+	 * Generate by pattern
+	 * 
+	 * @param string $pattern	pattern
+	 * @param array $values		values
+	 */
+	public static function generateByPattern($pattern, $values)
+	{
+		preg_match_all("/\[[^\]]*\]/", $text, $matches);
+	}
+	
+}
