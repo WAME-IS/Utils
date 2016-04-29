9272b02caf7d4a2e1956ee52b4c319273f33d2c5
shadd0w <rene.gmitter@wame.sk>
2016-04-29 12:57:22 +0200

    Update Utils

---
+++ Strings.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wame\Utils;
+
+class Strings
+{
+	static function truncate($string, $length=100, $append="&hellip;") {
+		$string = trim($string);
+
+		if(strlen($string) > $length) {
+			$string = wordwrap($string, $length);
+			$string = explode("\n", $string, 2);
+			$string = $string[0] . $append;
+		}
+
+		return $string;
+	}
+}
