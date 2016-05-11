dfd7c4ca5b705dcfcc62fa84f86b76fe39352d18
MacMini2 <djsucho@gmail.com>
2016-05-11 09:42:22 +0200

    add dashesToCamelCase string to utils

--- Strings.php
+++ Strings.php
@@ -15,4 +15,18 @@ class Strings
 
 		return $string;
 	}
+    
+    
+    static function dashesToCamelCase($string, $capitalizeFirstCharacter = false) 
+    {
+
+        $str = str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+
+        if (!$capitalizeFirstCharacter) {
+            $str[0] = strtolower($str[0]);
+        }
+
+        return $str;
+    }
+    
 }
