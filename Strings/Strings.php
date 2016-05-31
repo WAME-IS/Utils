34cdf1045d66fcfc1da73d3a4d51a486597ff695
shadd0w <rene.gmitter@wame.sk>
2016-05-31 11:12:27 +0200

    UPDATE ArticleModule

--- Strings/Strings.php
+++ Strings/Strings.php
@@ -60,4 +60,14 @@ class Strings
 		return $reflect->getShortName();
 	}
     
+	static function startsWith($haystack, $needle) {
+		// search backwards starting from haystack length characters from the end
+		return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
+	}
+
+	static function endsWith($haystack, $needle) {
+		// search forward starting from end minus needle length characters
+		return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
+	}
+	
 }
