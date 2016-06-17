cda9fc30380926af62bbdcadb77be819708a7647
shadd0w <rene.gmitter@wame.sk>
2016-06-17 09:52:53 +0200

    UPDATE Utils/Date

--- Date/Date.php
+++ Date/Date.php
@@ -13,7 +13,9 @@ class Date
 	 */
 	public static function toString($date, $format = 'Y-m-d H:i:s')
 	{
-        if ($date) {
+		if ($date == 'now') {
+			$return = \date($format);
+		} else if ($date) {
             $return = $date->format($format);
         } else {
             $return = '';
@@ -21,4 +23,21 @@ class Date
          
         return $return;
 	}
+	
+	/**
+	 * Format string date to DateTime for Doctrine entity
+	 * 
+	 * @param DateTime $date
+	 * @param string $format
+	 * @return DateTime
+	 */
+	public static function toDateTime($date, $format = 'Y-m-d H:i:s')
+	{
+		if ($date == 'now') {
+			return new DateTime('now');
+		} else {
+			return new DateTime(date($format, strtotime($date)));
+		}
+	}
+	
 }
