70a2f860ee7601aa07177562c3384d30186bf3ad
Linzee <d.gmiterko@gmail.com>
2016-07-30 22:14:29 +0200

    ADD DataLoader

--- Strings/Strings.php
+++ Strings/Strings.php
@@ -2,40 +2,42 @@
 
 namespace Wame\Utils;
 
+use Nette\Reflection\ClassType;
+use Nette\Utils\Strings;
 use Nette\Utils\Strings as NStrings;
 
 class Strings extends NStrings
 {
-	/**
-	 * Truncate string
-	 * 
-	 * @param string $string
-	 * @param int $length
-	 * @param string $append
-	 * @return string
-	 */
-	static function truncate($string, $length = 100, $append = '&hellip;') 
-	{
-		$string = trim($string);
 
-		if (strlen($string) > $length) {
-			$string = wordwrap($string, $length);
-			$string = explode("\n", $string, 2);
-			$string = $string[0] . $append;
-		}
+    /**
+     * Truncate string
+     * 
+     * @param string $string
+     * @param int $length
+     * @param string $append
+     * @return string
+     */
+    static function truncate($string, $length = 100, $append = '&hellip;')
+    {
+        $string = trim($string);
 
-		return $string;
-	}
-    
-    
-	/**
-	 * Convert string to CamelCase
-	 * 
-	 * @param string $string
-	 * @param boolean $capitalizeFirstCharacter
-	 * @return string
-	 */
-    static function dashesToCamelCase($string, $capitalizeFirstCharacter = false) 
+        if (strlen($string) > $length) {
+            $string = wordwrap($string, $length);
+            $string = explode("\n", $string, 2);
+            $string = $string[0] . $append;
+        }
+
+        return $string;
+    }
+
+    /**
+     * Convert string to CamelCase
+     * 
+     * @param string $string
+     * @param boolean $capitalizeFirstCharacter
+     * @return string
+     */
+    static function dashesToCamelCase($string, $capitalizeFirstCharacter = false)
     {
         $str = str_replace(' ', '', ucwords(str_replace('-', ' ', self::webalize($string, null, false))));
 
@@ -45,34 +47,44 @@ class Strings extends NStrings
 
         return $str;
     }
-	
-	
-	/**
-	 * Get class name from namespace
-	 * 
-	 * @param string $namespace
-	 * @param boolean $capitalizeFirstCharacter
-	 * @return string
-	 */
-	static function getClassName($namespace, $capitalizeFirstCharacter = true)
-	{
-		$reflect = new \ReflectionClass($namespace);
-		
-		$className = $reflect->getShortName();
-		
-		if (!$capitalizeFirstCharacter) {
+
+    /**
+     * Get class name from namespace
+     * 
+     * @param string $namespace
+     * @param boolean $capitalizeFirstCharacter
+     * @return string
+     */
+    static function getClassName($namespace, $capitalizeFirstCharacter = true)
+    {
+        $reflect = new ClassType($namespace);
+
+        $className = $reflect->getShortName();
+
+        if (!$capitalizeFirstCharacter) {
             $className[0] = strtolower($className[0]);
         }
-		
-		return $className;
-	}
-    
-    
+
+        return $className;
+    }
+
     static function parseTemplate($template, $parameters)
     {
-        return \Nette\Utils\Strings::replace($template, '!\{\{(\w+)\}\}!', function($m) use($parameters) {
-            return $parameters[$m[1]];
-        });
+        return Strings::replace($template, '!\{\{(\w+)\}\}!', function($m) use($parameters) {
+                return $parameters[$m[1]];
+            });
     }
-	
+
+    static function plural($string)
+    {
+        $last_letter = strtolower($string[strlen($string) - 1]);
+        switch ($last_letter) {
+            case 'y':
+                return substr($string, 0, -1) . 'ies';
+            case 's':
+                return $string . 'es';
+            default:
+                return $string . 's';
+        }
+    }
 }
