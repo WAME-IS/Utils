c27dd1cd816b2ae6d4b59fa5da8d7581fdf6f3ac
Linzee <d.gmiterko@gmail.com>
2016-08-25 11:41:24 +0200

    ADD TreeList

--- Tree/SimpleTreeSorter.php
+++ Tree/SimpleTreeSorter.php
@@ -2,41 +2,47 @@
 
 namespace Wame\Utils\Tree;
 
-class SimpleTreeSorter extends \Nette\Object {
+/**
+ * Class SimpleTreeSorter is used to convert array of items which have parent_id into tree. Each node in tree is represented as TreeNode.
+ * 
+ * @author Dominik Gmiterko <ienze@ienze.me>
+ */
+class SimpleTreeSorter extends \Nette\Object
+{
 
-	private $items;
+    /**
+     * Converts array into tree.
+     * 
+     * @param array $items Array of items
+     * @param string $parent Property of items containing a parent_id
+     * @param string $idColumn Proeprty of items containing id
+     * @param int $fromId Where to start converting of tree
+     * @return TreeNode Root tree node
+     */
+    public function sortTree($items, $parentColumn = 'parent_id', $idColumn = 'id', $fromId = 0)
+    {
+        $itemsChilds = [];
 
-	public function __construct($items) {
-		if ($items instanceof \Nette\Database\Table\Selection) {
-			$this->items = $items->fetchAll();
-		} else {
-			$this->items = $items;
-		}
-	}
+        foreach ($this->items as $item) {
+            $parentId = 0 + $item->$parentColumn;
+            $itemsChilds[$parentId][] = $item;
+        }
 
-	public function sortTree($parentColumn = 'parent_id', $idColumn = 'id', $fromId = 0) {
-		$itemsChilds = [];
+        return $this->build($itemsChilds, $idColumn, $fromId);
+    }
 
-		foreach ($this->items as $item) {
-			$parentId = 0 + $parentId = $item->$parentColumn;
-			$itemsChilds[$parentId][] = $item;
-		}
+    private function build($itemsChilds, $idColumn, $parentId)
+    {
+        $items = [];
 
-		return $this->build($itemsChilds, $idColumn, $fromId);
-	}
+        if (isset($itemsChilds[$parentId])) {
+            foreach ($itemsChilds[$parentId] as $item) {
+                $items[] = new TreeNode($item, $this->build($itemsChilds, $idColumn, $item->$idColumn));
+            }
 
-	private function build($itemsChilds, $idColumn, $parentId) {
-		$items = [];
+            return $items;
+        }
 
-		if (isset($itemsChilds[$parentId])) {
-			foreach ($itemsChilds[$parentId] as $item) {
-				
-				$items[] = new TreeNode($item, $this->build($itemsChilds, $idColumn, $item->$idColumn));
-			}
-
-			return $items;
-		}
-
-		return null;
-	}
+        return null;
+    }
 }
--- Tree/TreeNode.php
+++ Tree/TreeNode.php
@@ -3,66 +3,75 @@
 namespace Wame\Utils\Tree;
 
 use Nette\MemberAccessException,
-	Nette\Object,
-	Nette\Utils\ObjectMixin;
+    Nette\Utils\ObjectMixin;
 
-class TreeNode {
+/**
+ * Class representing one node of tree, property "childNodes" is mapped to array of child nodes. All other properties are mapped to current item properties.
+ * 
+ * @author Dominik Gmiterko <ienze@ienze.me>
+ */
+class TreeNode
+{
 
-	const CHILDS_NAME = 'child_nodes';
+    const CHILDS_NAME = 'childNodes';
 
-	private $item;
-	private $childs;
+    private $item;
+    private $childs;
 
-	public function __construct($item, $childs = null) {
-		$this->item = $item;
-		$this->childs = $childs ? $childs : [];
-	}
+    public function __construct($item, $childs = null)
+    {
+        $this->item = $item;
+        $this->childs = $childs ? $childs : [];
+    }
 
-	public function __call($name, $args) {
-		return call_user_func([$this->item, $name], $args);
-	}
+    public function __call($name, $args)
+    {
+        if ($name == 'get' . ucfirst(self::CHILDS_NAME)) {
+            return $this->childs;
+        }
+        return call_user_func([$this->item, $name], $args);
+    }
 
-	public function __get($name) {
-		if ($name != 'item') {
-			if ($name == self::CHILDS_NAME) {
-				return $this->childs;
-			} else {
-				return $this->item->$name;
-			}
-		} else {
-			return $this->$name;
-		}
-	}
+    public function __get($name)
+    {
+        if ($name == self::CHILDS_NAME) {
+            return $this->childs;
+        }
+        return $this->item->$name;
+    }
 
-	public function __set($name, $value) {
-		$this->item->$name = $value;
-	}
+    public function __set($name, $value)
+    {
+        $this->item->$name = $value;
+    }
 
-	/**
-	 * Is property defined?
-	 * @param  string  property name
-	 * @return bool
-	 */
-	public function __isset($name) {
-		return ObjectMixin::has($this->item, $name);
-	}
+    /**
+     * Is property defined?
+     * @param  string  property name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return ObjectMixin::has($this->item, $name);
+    }
 
-	/**
-	 * Access to undeclared property.
-	 * @param  string  property name
-	 * @return void
-	 * @throws MemberAccessException
-	 */
-	public function __unset($name) {
-		ObjectMixin::remove($this->item, $name);
-	}
+    /**
+     * Access to undeclared property.
+     * @param  string  property name
+     * @return void
+     * @throws MemberAccessException
+     */
+    public function __unset($name)
+    {
+        ObjectMixin::remove($this->item, $name);
+    }
 
-	public function __toString() {
-		if (is_scalar($this->item)) {
-			return $this->item;
-		} else {
-			return (string) $this->item;
-		}
-	}
-
+    public function __toString()
+    {
+        if (is_scalar($this->item)) {
+            return $this->item;
+        } else {
+            return (string) $this->item;
+        }
+    }
 }
