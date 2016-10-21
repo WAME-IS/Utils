7751292f2f35fc8bc0c78ffa1cab151d10ef052d
shadd0w <rene.gmitter@wame.sk>
2016-10-21 13:34:18 +0200

    ADD Arrays multi-array encode/decode methods

--- Array/Arrays.php
+++ Array/Arrays.php
@@ -36,5 +36,47 @@ class Arrays extends NArrays
         
         return $pairs;
     }
+    
+    /**
+     * Convert multiarray to nice-looking string
+     * 
+     * example:
+     * from: [color => [blue, red]], to: "color-blue,red"
+     * 
+     * @param type $array   array
+     * @return type
+     */
+    static public function encodeMultiArray(array $array, $s1 = ":", $s2 = ",", $s3 = ";")
+    {
+        $p = [];
+        
+        foreach($array as $i => $values) {
+            if(is_array($values)) {
+                $p[] = sprintf("%s$s1%s", $i, implode($s2, array_map(function($v) { return $v; }, $values)));
+            }
+        }
+        
+        return implode($s3, $p);
+    }
+    
+    /**
+     * 
+     * @param type $string
+     * @param type $s1
+     * @param type $s2
+     * @param type $s3
+     */
+    static public function decodeMultiArray($string, $s1 = ":", $s2 = ",", $s3 = ";")
+    {
+        $values = explode($s3, $string);
+
+        $arr = [];
+        foreach($values as $val) {
+            $parts = explode($s1, $val);
+            $arr[$parts[0]] = explode($s2, $parts[1]);
+        }
+        
+        return $arr;
+    }
 
 }
