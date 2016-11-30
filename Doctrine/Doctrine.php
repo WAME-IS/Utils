c88e18dc2ef58e41598fae33347c971336a961f8
shadd0w <rene.gmitter@wame.sk>
2016-11-30 17:13:51 +0100

    UPDATE Utils/Doctrine

--- Doctrine/Doctrine.php
+++ Doctrine/Doctrine.php
@@ -14,27 +14,29 @@ class Doctrine
      * @param string $para      parameter
      * @return QueryBuilder
      */
-    public static function removeWherePart($qb, $part, $para = null)
+    public static function removeWherePart($qb, $part, $para = [])
     {
         $qbWherePart = $qb->getDqlPart('where')->getParts();
         $qb->resetDQLPart('where');
         
         foreach ($qbWherePart as $whereClause) {
-            if ($whereClause === $part || ($whereClause instanceof Comparison && $whereClause->getLeftExpr() === $part)) {
+            if (strpos($whereClause, $part) !== false || ($whereClause instanceof Comparison && strpos($whereClause->getLeftExpr(), $part) !== false)) {
                 continue;
             }
             $qb->andWhere($whereClause);
         }
         
-        $params = $qb->getParameters();
-        
-        foreach ($params as $key => $param) {
-            if ($param->getName() === $para) {
-                $params->remove($key);
+        if(!empty($para)) {
+            $params = $qb->getParameters();
+
+            foreach ($params as $key => $param) {
+                if (in_array($param->getName(), $para)) {
+                    $params->remove($key);
+                }
             }
+
+            $qb->setParameters($params);
         }
-        
-        $qb->setParameters($params);
         
         return $qb;
     }
