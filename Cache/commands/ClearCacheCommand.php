92727ca69b7dd5d06ae1d1cd96e8a6972971f489
Matej Soroka <msoroka27@gmail.com>
2016-12-30 09:53:31 +0100

    CREATE ClearCacheCommand

---
+++ Cache/commands/ClearCacheCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Wame\Core\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Wame\Utils\File\FileHelper;
+
+class ClearCacheCommand extends Command
+{
+    /** {@inheritDoc} */
+    protected function configure()
+    {
+        $this
+            ->setName('nette:clear-cache:all')
+            ->setDescription('Clear Nette cache.');
+    }
+
+    /** {@inheritdoc} */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            FileHelper::emptyDir(TEMP_PATH . '/cache');
+            FileHelper::emptyDir(TEMP_PATH . '/entities');
+            FileHelper::emptyDir(TEMP_PATH . '/proxies');
+
+            $output->writeln('Cache successfully cleared');
+
+            return 0; // zero return code means everything is ok
+        } catch (\Exception $exc) {
+            $output->writeln("<error>{$exc->getMessage()}</error>");
+
+            return 1; // non-zero return code means error
+        }
+    }
+
+}
